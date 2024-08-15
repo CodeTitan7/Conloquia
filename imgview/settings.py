30a585fa27d3a96df20a932c0601eaa5bd2f04fc
V DEVANARAYAN <89205573+CodeTitan7@users.noreply.github.com>
2024-08-15 21:30:32 +0530

    Update settings.py

--- imgview/settings.py
+++ imgview/settings.py
@@ -128,7 +128,6 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='kingaugustus78@gmail.com'
 EMAIL_HOST_PASSWORD='qssw wrhx ofiu hiyh'
->>>>>>> 37a92a6fc3f6ee25e264e6794cda68e02445fee4
 
 
 DEFAULT_EMAIL = 'default@example.com'
